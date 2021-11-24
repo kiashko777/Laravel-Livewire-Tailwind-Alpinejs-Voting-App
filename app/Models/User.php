e029f8d5a491ff62fdbb6be322ede34fad11f091
kiashko777 <kiashko777@gmail.com>
2021-11-24 11:26:13 +0400

    chore:implement gravatars
    functionality to get gravatars implemented and tested

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,25 @@ class User extends Authenticatable
   {
     return $this->hasMany(Idea::class);
   }
+
+
+  //METHOD TO GET AVATAR
+
+  public function getAvatar()
+  {
+    $firstCharacter = $this->email[0];
+
+    if (is_numeric($firstCharacter)) {
+      $integerToUse = ord(strtolower($firstCharacter)) - 21;
+    } else {
+      $integerToUse = ord(strtolower($firstCharacter)) - 96;
+    }
+
+    return 'https://www.gravatar.com/avatar/'
+      . md5($this->email)
+      . '?s=200'
+      . '&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+      . $integerToUse
+      . '.png';
+  }
 }
