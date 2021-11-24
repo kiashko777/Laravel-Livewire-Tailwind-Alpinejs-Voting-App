c07d5d9e31824d59ed3b38c8b3b7b37048e044b9
kiashko777 <kiashko777@gmail.com>
2021-11-24 18:26:25 +0400

    feat:livewire idea create form
    implemented functionality for idea creattion,livewire form created and tested

---
+++ app/Http/Livewire/CreateIdea.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Category;
+use App\Models\Idea;
+use Illuminate\Http\Response;
+use Livewire\Component;
+
+class CreateIdea extends Component
+{
+
+  //METHOD TO CREATE AND VALIDATE AN IDEA
+
+  public $title;
+  public $category = 1;
+  public $description;
+
+  protected $rules = [
+    'title' => 'required|min:4',
+    'category' => 'required|integer',
+    'description' => 'required|min:10',
+  ];
+
+
+  public function createIdea()
+  {
+    $this->validate();
+
+    if (auth()->check()) {
+      Idea::create([
+        'user_id' => auth()->id(),
+        'category_id' => $this->category,
+        'status_id' => 1,
+        'title' => $this->title,
+        'description' => $this->description,
+      ]);
+
+      session()->flash('success_message', 'Idea was added successfully!');
+      $this->reset();
+      return redirect()->route('idea.index');
+    }
+    abort(Response::HTTP_FORBIDDEN);
+  }
+
+
+  public function render()
+  {
+    //METHOD TO RENDER THE FORM
+
+    return view('livewire.create-idea', [
+      'categories' => Category::all()
+    ]);
+  }
+}
